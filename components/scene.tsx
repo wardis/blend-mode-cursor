b75bde5022add21d0329843fe1839762d55ce134
Wardi Soulaimana <wardi@wardi-s.com>
2024-03-17 03:45:21 +0000

    init

---
+++ components/scene.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React from 'react'
+
+export default function Scene() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-[4.5vw] max-w-[900] text-center z-50 p-20">
+        The quick brown fox jumps over the lazy dog
+      </h1>
+    </div>
+  )
+}
